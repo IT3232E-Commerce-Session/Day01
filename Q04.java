e8573555c3e749817f3e4e31e02273d1b6f5714e
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-03-18 18:49:53 +0000

    odd numbers from 1 to 20 is printed

---
+++ Q04.java
@@ -0,0 +1,25 @@
+// 4)print the odd numbers between 1 to 20
+
+public class Q04{
+	public static void main(String[] args){
+		int i;
+	for ( i=1; i<=20; i++){
+        if(i%2 == 1)
+	        System.out.println(i);
+	}
+	}
+}
+
+// output
+// @T-Lanojika ➜ /workspaces/Day01 (main) $ javac Q04.java
+// @T-Lanojika ➜ /workspaces/Day01 (main) $ java Q04
+// 1
+// 3
+// 5
+// 7
+// 9
+// 11
+// 13
+// 15
+// 17
+// 19
