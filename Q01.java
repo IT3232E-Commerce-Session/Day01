3906dbd30e07645627cb2eed3bad686201ec513b
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-03-18 18:34:14 +0000

    output is inserted

---
+++ Q01.java
@@ -0,0 +1,12 @@
+//print a message
+
+public class Q01{
+public static void main(String[] args){
+System.out.println("Hello World...");
+}
+}
+
+//output
+// @T-Lanojika ➜ /workspaces/Day01 (main) $ javac Q01.java
+// @T-Lanojika ➜ /workspaces/Day01 (main) $ java Q01
+// Hello World...
