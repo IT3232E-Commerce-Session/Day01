aa45c7768a851b32af1c3ac8a69410643c682d9d
Thayaparan Lanojika <128678015+T-Lanojika@users.noreply.github.com>
2025-03-18 18:45:14 +0000

    print numbers in reverse is done

---
+++ Q03.java
@@ -0,0 +1,18 @@
+// 3)print the numbers in reverse
+
+public class Q03 {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
+            System.out.println(i);
+        }
+    }
+}
+
+// output
+// @T-Lanojika ➜ /workspaces/Day01 (main) $ javac Q03.java
+// @T-Lanojika ➜ /workspaces/Day01 (main) $ java Q03
+// 5
+// 4
+// 3
+// 2
+// 1
